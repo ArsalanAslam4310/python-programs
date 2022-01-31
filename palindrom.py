bc3eea068255662baaf656b38f807031b22c1df6
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-01-31 23:26:26 +0500

    Add files via upload

---
+++ palindrom.py
@@ -0,0 +1,6 @@
+a = input("Enter a list:")
+
+if  a==a[::-1]:
+    print("Palindrom")
+else:
+    print("Not a palindrom")
---
+++ while_loop.py
@@ -0,0 +1,8 @@
+numbers =[2,25,5,3,5]
+sum =0
+i = 0
+
+while sum < len(numbers):
+    sum = sum + numbers[i]
+    i +=1
+print(sum)
