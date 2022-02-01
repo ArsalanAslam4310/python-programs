2869abd3b8741968e387806dfd337bf99164b331
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-01 12:36:33 +0500

    Update and rename divide .py to prime.py

---
+++ prime.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+number = int(input("Enter a number"))
+flag = False
+if number <= 1:
+    print(f"number{number} is not prime")
+else:
+    for i in range(int(round(sqrt(number)))+1):
+        if i >= 2:
+            if number % i == 0:
+                flag = True
+                break
+    if flag:
+        print(f"The number {number} is not prime")
+    else:
+        print(f"The number {number} is prime")
