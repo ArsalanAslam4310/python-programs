69dc6fd1420ac77a98dff078e0078f830af9e931
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-08 10:29:39 +0500

    Update element_occur.py

--- element_occur.py
+++ element_occur.py
@@ -1,11 +1,9 @@
-def occur_in_list(list_of_numbers):
-    number = int(input("Enter a number:"))
+def occur_in_list(list_of_numbers,number):
     if number in list_of_numbers:
         print("yes")
     else:
         print("Not")
-    return number
 
 list_of_numbers = [55,44,8,7,2,1]
-occur= occur_in_list(list_of_numbers)
-print(occur)
+number = int(input("Enter a number:"))
+occur_in_list(list_of_numbers,number)
